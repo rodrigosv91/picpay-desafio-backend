4463934cc221e4f1d6884f833cfb56f21b8d0668
rodrigo <rodrigosv91@gmail.com>
2024-03-27 20:06:24 -0300

    initial config

---
+++ src/main/java/br/com/rdsv/picpaydesafiobackend/PicpayDesafioBackendApplication.java
@@ -0,0 +1,13 @@
+package br.com.rdsv.picpaydesafiobackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PicpayDesafioBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PicpayDesafioBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/rdsv/picpaydesafiobackend/PicpayDesafioBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.rdsv.picpaydesafiobackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PicpayDesafioBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
