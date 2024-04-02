0470114faa13ba5010cd029e375c54fc937e2a10
rodrigo <rodrigosv91@gmail.com>
2024-04-01 22:32:20 -0300

    refactor: change table names

--- src/main/java/br/com/rdsv/picpaydesafiobackend/transaction/Transaction.java
+++ src/main/java/br/com/rdsv/picpaydesafiobackend/transaction/Transaction.java
@@ -7,7 +7,7 @@ import org.springframework.data.relational.core.mapping.Table;
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 
-@Table("TRANSACTION")
+@Table("TRANSACTIONS")
 public record Transaction(
         @Id Long id,
         Long payer,
--- src/main/java/br/com/rdsv/picpaydesafiobackend/wallet/Wallet.java
+++ src/main/java/br/com/rdsv/picpaydesafiobackend/wallet/Wallet.java
@@ -1,9 +1,11 @@
 package br.com.rdsv.picpaydesafiobackend.wallet;
 
 import org.springframework.data.annotation.Id;
+import org.springframework.data.relational.core.mapping.Table;
 
 import java.math.BigDecimal;
 
+@Table("WALLETS")
 public record Wallet(
         @Id Long id,
         String fullName,
